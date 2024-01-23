5b30796534b5a5c73d0603efd40ce43de9846050
Bảo Bảo <thanh.bao@outlook.com>
2024-01-23 10:36:44 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div style={{ margin: 99 , backgroundColor: 'white'}}>
       <div style={{ margin: 99 , backgroundColor: 'white', padding: 99}}>
-        <QRCode value="00020101021238570010A000000727012700069704220113VQRQ000198ram0208QRIBFTTA5303704540450005802VN62200816TTBN52ZD COMSUON63040950" />
+        <QRCode value="00020101021238570010A000000727012700069704220113VQRQ0001990hq0208QRIBFTTA5303704540450005802VN62200816TTJV27GB COMSUON63044E27" />
       </div>
     </div>
   )
